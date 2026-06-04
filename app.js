d5a315942ac9085bb842c55c1f0268c88bc76de7
Yuriy Dybskiy <yuriy@pumabrowser.com>
2026-06-03 23:53:09 -0700

    Refresh atlas styling for a sleeker civic editorial look

--- app.js
+++ app.js
@@ -86,8 +86,8 @@ function revealElements() {
       });
     },
     {
-      threshold: 0.12,
-      rootMargin: '0px 0px -40px 0px',
+      threshold: 0.04,
+      rootMargin: '0px',
     }
   );
 
